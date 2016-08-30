24ddd654ddf76b6ae22c40674cf7066b3d1e620b
Kysamaa <Alex.zwiebel@web.de>
2016-08-30 10:34:35 +0200

    6.17

--- Aka´s Draven/Properties/AssemblyInfo.cs
+++ Aka´s Draven/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.16.0.0")]
-[assembly: AssemblyFileVersion("6.16.0.0")]
+[assembly: AssemblyVersion("6.17.0.0")]
+[assembly: AssemblyFileVersion("6.17.0.0")]
